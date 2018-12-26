90606519a3946e7a23e829f622fce1fbedf5dee7
Tudor Golubenco <tudor@elastic.co>
2018-12-26 12:33:21 +0200

    tiny refactor

--- x-pack/plugins/siem_rules/rules_service.ts
+++ x-pack/plugins/siem_rules/rules_service.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import nodeCrypto from '@elastic/node-crypto';
 import Joi from 'joi';
 
 interface ESQueryRuleParams {
@@ -16,11 +17,13 @@ interface ESQueryRuleParams {
 export class RulesService {
   private kbnServer: any;
   private taskManager: TaskManager;
+  private crypto: any;
 
   constructor(kbnServer: any) {
     this.kbnServer = kbnServer;
-
     const server = this.kbnServer.server;
+    this.kbnServer.crypto = cryptoFn(server);
+
     server.route({
       method: 'GET',
       path: '/api/siem_rules/tasks',
@@ -48,7 +51,7 @@ export class RulesService {
       },
       handler: async (request: any) => {
         this.info(`Request payload: ${request.payload}`);
-        return await this.scheduleRule(request.payload);
+        return await this.scheduleRule(request.payload, request.headers);
       },
     });
 
@@ -102,7 +105,10 @@ export class RulesService {
     });
   }
 
-  public async scheduleRule(payload: ESQueryRuleParams) {
+  public async scheduleRule(payload: ESQueryRuleParams, headers: any[]) {
+    this.info('schedule Headers: ' + JSON.stringify(headers));
+    const encryptedHeaders = await this.kbnServer.crypto.encrypt(JSON.stringify(headers));
+    this.info('encryptedHeaders: ' + encryptedHeaders);
     this.info(`scheduling task ${payload.name}`);
     return await this.taskManager.schedule({
       taskType: 'siemRuleQuery',
@@ -111,6 +117,7 @@ export class RulesService {
       scope: 'siem_rules',
       params: {
         payload,
+        encryptedHeaders,
       },
     });
   }
@@ -124,10 +131,29 @@ export class RulesService {
       createTaskRunner(context: RunContext) {
         return {
           async run() {
-            const { name, query } = context.taskInstance.params.payload;
+            const payload: ESQueryRuleParams = context.taskInstance.params.payload;
             context.kbnServer.server.log(
               ['info', 'siem-rules-service'],
-              `Task runnning: ${name}. Query: ${query}`
+              `Task runnning: ${payload.name}. Query: ${payload.query}`
+            );
+
+            const { callWithRequest } = context.kbnServer.server.plugins.elasticsearch.getCluster(
+              'data'
+            );
+            const req = await getRequestContext(
+              context.taskInstance.params.encryptedHeaders,
+              context.kbnServer,
+              ''
+            );
+
+            const result = await callWithRequest(req, 'search', {
+              index: payload.indexPattern,
+              ignoreUnavailable: true,
+              q: payload.query,
+            });
+            context.kbnServer.server.log(
+              ['info', 'siem-rules-service'],
+              `Result: ` + JSON.stringify(result)
             );
           },
 
@@ -146,4 +172,31 @@ export class RulesService {
   private info(message: string) {
     this.kbnServer.server.log(['info', 'siem-rules-service'], message);
   }
+}
+
+function cryptoFn(server) {
+  const encryptionKey = server.config().get('xpack.security.encryptionKey');
+  return nodeCrypto({ encryptionKey });
+}
+
+async function getRequestContext(encryptedHeaders: string, kbnServer: any, basepath: string) {
+  const headers = await kbnServer.crypto.decrypt(encryptedHeaders);
+
+  const server: KbnServer = kbnServer.server;
+  const serverBasePath: string = server.config().get('server.basePath');
+
+  const fakeRequest: any = {
+    headers: JSON.parse(headers),
+    // This is used by the spaces SavedObjectClientWrapper to determine the existing space.
+    // We use the basePath from the saved job, which we'll have post spaces being implemented;
+    // or we use the server base path, which uses the default space
+    getBasePath: () => basePath || serverBasePath,
+  };
+
+  if (server.plugins.security) {
+    const { authorization } = server.plugins.security;
+    await authorization.mode.initialize(fakeRequest);
+  }
+
+  return fakeRequest;
 }
